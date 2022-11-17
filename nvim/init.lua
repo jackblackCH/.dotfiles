c5e6f69bca417a009cf27059feaf196687e0f5ec
Marc Illien <marc@unique.ch>
2022-11-17 19:03:54 +0100

    Add nvim

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require("pharao.plugins-setup")
+require("pharao.core.options")
+require("pharao.core.keymaps")
+require("pharao.core.colorscheme")
---
+++ nvim/lua/pharao/core/keymaps.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+-- general keymaps
+
+keymap.set("i", "jk", "<ESC>")
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<leader>se", "<C-w>=")
+keymap.set("n", "<leader>sx", ":close<CR>")
+
+
+keymap.set("n", "<leader>sx", ":close<CR>")
